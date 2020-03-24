f9b3d2154c56c3dfe77753e44d271a33f25e769c
Sbhawal <mailtosnehaan@gmail.com>
2020-03-25 03:26:46 +0530

    program 7

---
+++ 10001st_prime.py
@@ -0,0 +1,27 @@
+def isPrime(n):
+    if (n <= 1):
+        return False
+    if (n <= 3):
+        return True
+    if (n % 2 == 0 or n % 3 == 0):
+        return False
+    i = 5
+    while (i * i <= n):
+        if (n % i == 0 or n % (i + 2) == 0):
+            return False
+        i = i + 6
+    return True
+
+def sol(limit):
+    c = 2
+    n = 0
+    while c < limit:
+        n+= 6
+        if isPrime(n + 1):
+            c+=1
+
+        if isPrime(n - 1):
+            c+=1
+    return n + 1
+
+print(sol(10001))
